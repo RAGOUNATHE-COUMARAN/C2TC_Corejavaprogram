3c7fb398925098533f71e97c491faf504c04207f
RAGOU-CHITRA <ragounathe007@gmail.com>
2025-07-21 22:31:33 +0530

    assignment is completed

---
+++ C2TC_Corejavaprogram/src/Day2/HW/WhileDemo.java
@@ -0,0 +1,16 @@
+package Day2.HW;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    
+		int start=99;
+		while(start<=100)
+		{
+			System.out.println(start);
+			start++;
+		}
+	}
+
+}
