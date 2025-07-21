50d9a0e70bc27cb21979721412c4e999143547dc
RAGOU-CHITRA <ragounathe007@gmail.com>
2025-07-21 14:24:45 +0530

    2nd problem solved

--- C2TC_Corejavaprogram/src/day1/basics/SampleDemo.java
+++ C2TC_Corejavaprogram/src/day1/basics/SampleDemo.java
@@ -1,10 +1,22 @@
 package day1.basics;
 
+
+
 public class SampleDemo {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to JFS....");
-	}
-
+	
+	public static void main(char c [])
+{
+	System.out.println("welcome to the main 2");
 }
+public void main (String ar )
+{
+	System.out.println("welcome to main 3");
+}
+public static void main() {
+	System.out.println("welcome to pack 1");
+}
+public static void main(String[] args) {
+	
+	System.out.println("Welcome to JFS....");
+	System.out.println("next line");
+}}
